ca6bdd603539b6a1b2bd0fbb5636047fb75f98ee
Li-HONGYAO <lihy_online@163.com>
2020-10-26 21:28:49 +0800

    1. 优化提示;  2. 优化代码

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@
 
 // 添加命令的库
 const program = require('commander');
+// 工具库
+const { cleanArgs } = require('../utils');
 
 
 program
-  .version(`Version is ${require('../package.json').version}`)
-  .description('lg-cli 帮助指南')
+  .version(`当前版本：${require('../package.json').version}`)
+  .description('Hello，我是耀哥，欢迎使用「lg-umijs-cli」')
   .usage('<command> [options]');
 
 program
   .command('create <app-name>')
-  .description('创建新项目')
-  .option('-f, --force', 'Overwrite target directory if it exists')
-  .option('-c, --clone', 'Use git clone when fetching remote preset')
+  .description('创建项目')
+  .option('-f, --force', '如果目录下已存在目标项目，则覆盖它')
   .action((name, cmd) => {
     const options = cleanArgs(cmd);
     require('../lib/create')(name, options)
@@ -22,17 +23,4 @@ program
 // 解析命令行参数
 program.parse(process.argv);
 
-// 工具函数
-function camelize(str) {
-  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
-}
-function cleanArgs(cmd) {
-  const args = {}
-  cmd.options.forEach(o => {
-    const key = camelize(o.long.replace(/^--/, ''))
-    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
-      args[key] = cmd[key]
-    }
-  })
-  return args
-}
+
---
+++ utils/index.js
@@ -0,0 +1,58 @@
+const logSymbols = require('log-symbols');
+const fs = require('fs');
+const { promisify } = require('util');
+const download = promisify(require('download-git-repo'));
+const ora = require('ora'); // 进度显示工具
+const chalk = require('chalk'); // 颜色显示工具
+
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
+
+function cleanArgs(cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
+
+function createDir(appName) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(`./${appName}`, function (err) {
+      if (err) {
+        console.log(logSymbols.err, '项目目录创建失败');
+        reject();
+      } else {
+        console.log(logSymbols.success, '项目目录创建成功');
+        resolve();
+      }
+    })
+  })
+}
+
+async function clone(remote, appName, options = { clone: true }) {
+  const process = ora(`开始下载项目模板 ${chalk.blue(remote)}`);
+  process.start();
+  process.color = 'yellow';
+  process.text = `正在下载项目模板... ${chalk.yellow(remote)}`;
+  download(remote, appName, options, (err) => {
+    if (err) {
+      process.color = "red";
+      process.text = `项目模板下载失败 ${chalk.green(remote)}`;
+      process.fail();
+    } else {
+      process.color = "green";
+      process.text = `项目模板下载成功 ${chalk.green(remote)}`;
+      process.succeed();
+    }
+  })
+}
+module.exports = {
+  cleanArgs,
+  createDir,
+  clone
+}
